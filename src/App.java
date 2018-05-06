d28f80c640892a621cba7e480ec7dbad017c7963
JM Fantin <joao.fantin@acad.pucrs.br>
2018-05-06 15:39:09 -0300

    Alterações em prints apenas

--- src/App.java
+++ src/App.java
@@ -10,19 +10,21 @@ public class App {
     
   //Recebe a lista de processos populada e lê seus valores
   	public static void imprimeVetor(Processo[] listaProcessos) {
-  		System.out.print("Processos:\t");
+  		System.out.print("Processo:\t");
   		for(int i = 1; i <= listaProcessos.length; i++){
   			System.out.print("P"+ i +"\t");
   		}
   		
-  		System.out.print("\nChegadas:\t");
+  		System.out.print("\nChegada:\t");
   		for(int i = 0; i < listaProcessos.length; i++){
   			System.out.print("["+listaProcessos[i].getChegada()+"]\t");
   		}
-  		System.out.print("\nExecuções:\t");
+  		
+  		System.out.print("\nExecução:\t");
   		for(int i = 0; i < listaProcessos.length; i++){
   			System.out.print("["+listaProcessos[i].getExecucao()+"]\t");
   		}
+  		
   		System.out.print("\nPrioridade:\t");
   		for(int i = 0; i < listaProcessos.length; i++){
   			System.out.print("["+listaProcessos[i].getPrioridade()+"]\t");
